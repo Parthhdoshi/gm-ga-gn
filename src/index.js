2dcacdb29d54c58ff19e8bed4a47bccb15405e9e
myname <you@example.com>
2022-01-08 16:38:54 +0530

    2nd commit

--- src/index.js
+++ src/index.js
@@ -4,11 +4,30 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+let curDate = new Date(2020,5,5,20);
+curDate = curDate.getHours();
+
+let greeting = "";
+let cssStyle = {};
+
+if(curDate >= 0 && curDate<12){
+  greeting = "Good Moring";
+  cssStyle.color = 'Green';
+} else if(curDate >= 12 && curDate<19){
+  greeting = "Good Afternoon";
+  cssStyle.color = 'Red';
+} else {
+greeting = "Good Night";
+cssStyle.color = 'blue';
+}
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+  <h1>Hello sir, <span style={cssStyle}>{greeting}</span></h1>
+  </>,
   document.getElementById('root')
+
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
